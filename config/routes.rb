028ff78572833baa375fc7cb8ce80985edb070ab
Katja Demas Glaser <katjaglaeser@icloud.com>
2022-06-02 15:57:14 +0200

    19 examples don route modelse

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :movies
+  #   resources :bookmarks, only: [ :new, :create, :destroy ]
+  # end
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
