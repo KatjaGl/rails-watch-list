2fd1140d552e983f0d73785c168e5edbdfe5496c
Katja Demas Glaser <katjaglaeser@icloud.com>
2022-06-03 19:57:42 +0200

    new 03juin2022 Aussehen noch zu bearbeiten

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,13 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
-
   end
 
   def show
     @bookmark = Bookmark.new
-    @list = List.find(params[:id])
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -17,19 +17,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      flash[:notice] = "List '#{@list.name}' has been successfully created"
-      redirect_to @list # = cocktail_path(@cocktail)
+      redirect_to list_path(@list)
     else
-      # le flash affichera un bandeau rouge et un message d'erreur.
-      flash[:alert] = "List '#{@list.name}' cannot be created. Check your inputs and try again."
       render :new
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
   end
-
 end
